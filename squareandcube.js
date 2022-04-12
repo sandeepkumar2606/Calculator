e41053ff1640c15c807babb33ca61ed007a65dfa
SandeepKumar <sandeep.kharinta200@gmail.com>
2022-04-12 12:07:54 +0530

    add the index,scriptand style files

---
+++ squareandcube.js
@@ -0,0 +1,10 @@
+function sq_cube() {
+
+            document.write("<table border=border><tr><th>Number</th><th>Square</th><th>Cube</th></tr>");
+
+            for(var i=0; i<=10; i++)
+            {
+                document.write("<tr><td>" + i + "</td><td>" + i*i + "</td><td>" + n*n*n "</td></tr>");
+                document.write("</table>");         
+            }
+        }
